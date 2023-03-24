990cc1e3d2b1d48d1463ec94a22bac3c2cdb7891
Lucas Berger <lucask.berger@gmail.com>
2023-03-24 17:39:57 +0100

    first draft

---
+++ src/parallax.ts
@@ -0,0 +1,27 @@
+import { Ref, ref, onMounted, onBeforeUnmount } from "vue";
+
+export const useContinuousParallax = (compRef: Ref<HTMLElement>) => {
+  const x = ref(0);
+
+  const scrollListener = () => {
+    const parent = compRef.value.parentElement;
+    const newVal =
+      (parent?.offsetHeight! * 0.7 +
+        parent?.scrollTop! -
+        compRef.value.offsetTop) /
+      compRef.value.offsetHeight;
+
+    x.value = Math.min(1.0, Math.max(newVal, 0.0));
+  };
+
+  onMounted(() => {
+    compRef.value.parentElement?.addEventListener("scroll", scrollListener);
+    scrollListener();
+  });
+
+  onBeforeUnmount(() => {
+    compRef.value.parentElement?.removeEventListener(scrollListener);
+  });
+
+  return x;
+};
